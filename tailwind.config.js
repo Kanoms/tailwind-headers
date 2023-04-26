feff5f4779dd4fc4dfb9bdc2e9b41dd67d60e2ce
Kelechi <kanomneze@gmail.com>
2023-04-26 17:13:03 +0100

    nav-bar and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lGreen: "#25DAC5",
+        lHeading: "#1e0e62",
+        lGray: "#808080",
+        lBlue: "#2F1893",
+        lPush: "#E93A7D",
+        lred: "#EA3223",
+      },
+      fontFamily: {
+        dmSans: ["DM Sans", "sans-serif"],
+      },
+    },
   },
-  plugins: [require('daisyui'),],
-}
-
+  plugins: [require("daisyui")],
+};
